88e9f7dd4fc4e185c4cd6c21f427cab8ace2e101
Cameron Valdez <cameronav23@gmail.com>
2020-11-24 16:31:05 -0600

    Initial commit

---
+++ Assets/Scripts/Room.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Farsight/Room")]
+public class Room : ScriptableObject
+{
+    [TextArea]
+    public string description;
+    public string roomName;
+    public string sound;
+    public string soundTake;
+    public Exit[] exits;
+    public InteractableObject[] interactableObjectsInRoom;
+
+}
